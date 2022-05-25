0ec91206efde79d6d2e71e8a4c4fb0d6f84c0598
Jinu <pjw019472@gmail.com>
2022-05-25 22:17:25 +0900

    coin info ui update

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -24,8 +24,99 @@ const Loader = styled.span`
   text-align: center;
 `;
 
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CoinInfo = styled.div`
+  display: flex;
+  width: 500px;
+  padding: 15px;
+  margin-bottom: 20px;
+  background-color: black;
+  border-radius: 15px;
+  justify-content: space-between;
+  align-items: center;
+  span:first-child {
+    text-transform: uppercase;
+    font-size: 13px;
+  }
+`;
+
+const CoinCol = styled.div`
+  display: flex;
+  height: 40px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const CoinDescription = styled.div`
+  width: 500px;
+  font-size: 18px;
+  line-height: 22px;
+  margin-bottom: 20px;
+`;
+
 interface RouteState {
   name: string;
+}
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
 }
 
 function Coin() {
@@ -33,8 +124,8 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const location = useLocation();
   const state = location.state as RouteState;
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -47,6 +138,7 @@ function Coin() {
       console.log(priceData);
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
   }, []);
   return (
@@ -54,7 +146,39 @@ function Coin() {
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <span></span>}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <Info>
+          <CoinInfo>
+            <CoinCol>
+              <span>rank:</span>
+              <span>{info?.rank}</span>
+            </CoinCol>
+            <CoinCol>
+              <span>symbol:</span>
+              <span>${info?.symbol}</span>
+            </CoinCol>
+            <CoinCol>
+              <span>open source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </CoinCol>
+          </CoinInfo>
+          <CoinDescription>
+            <p>{info?.description}</p>
+          </CoinDescription>
+          <CoinInfo>
+            <CoinCol>
+              <span>total supply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </CoinCol>
+            <CoinCol>
+              <span>max supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </CoinCol>
+          </CoinInfo>
+        </Info>
+      )}
     </Container>
   );
 }
